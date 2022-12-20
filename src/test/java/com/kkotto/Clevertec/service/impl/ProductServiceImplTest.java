b3b93d9dac965c93cbee9b99f9a44bc62a5fb35d
kkotto <ksen.sharupich.work@gmail.com>
2022-12-20 20:58:40 +0300

    createProduct test added

---
+++ src/test/java/com/kkotto/Clevertec/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.kkotto.Clevertec.service.impl;
+
+import com.kkotto.Clevertec.repository.ProductRepository;
+import com.kkotto.Clevertec.service.model.entity.Product;
+import com.kkotto.Clevertec.service.model.response.ProductDto;
+import liquibase.configuration.LiquibaseConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+    @Autowired
+    ProductServiceImpl productService;
+
+    @MockBean
+    ProductRepository productRepository;
+
+    @Test
+    void isProductCreatedWhenAlright(){
+        ProductDto productDtoTest = new ProductDto.Builder()
+                .name("Test-name")
+                .price(BigDecimal.valueOf(10))
+                .build();
+        Product product = new Product();
+        product.setName(productDtoTest.getName());
+        product.setPrice(productDtoTest.getPrice());
+        Mockito.when(productRepository.save(Mockito.any(Product.class))).thenReturn(product);
+        ProductDto product1 = productService.createProduct(productDtoTest);
+        Assertions.assertEquals(productDtoTest.getName(), product1.getName());
+    }
+}
